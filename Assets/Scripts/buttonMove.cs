cdb4d99f7564f955996a9afb4e250973e5edf627
enersesian <en6@njit.edu>
2019-04-27 12:37:36 -0400

    Initializing project. Build settings to android. Sample scene has the three stations with a button

---
+++ Assets/Scripts/buttonMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonMove : MonoBehaviour
+{
+    public GameObject button;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if (transform.localPosition.z < 0f) transform.localPosition = Vector3.zero;
+        if (transform.localPosition.z > 0.5f)
+        {
+            transform.localPosition = Vector3.zero;
+            //update task window
+        }
+        button.transform.localPosition = transform.localPosition;
+    }
+}
