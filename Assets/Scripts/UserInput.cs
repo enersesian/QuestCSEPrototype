ba87ea526515b2f30fb467430abca87c3a3a5902
Eric Nersesian <eric.nersesian@njit.edu>
2019-05-07 11:41:20 -0400

    Cleaned up protoype scene, created User and Station Manager objects, and userInput script

---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public float movementSpeed = 0.01f;
+
+    private Vector2 rightThumbAxis;
+    private float heightChange = 0f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rightThumbAxis = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick) * movementSpeed;
+
+        if (OVRInput.Get(OVRInput.Button.One)) //Right-hand A button
+        {
+            heightChange = -1f * movementSpeed;
+        }
+
+        else if (OVRInput.Get(OVRInput.Button.Two)) //Right-hand B button
+        {
+            heightChange = 1f * movementSpeed;
+        }
+
+        else heightChange = 0f;
+
+        transform.position = new Vector3(transform.position.x + rightThumbAxis.x, transform.position.y + heightChange, transform.position.z + rightThumbAxis.y);
+    }
+}
