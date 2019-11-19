ef5735d33d31ee58912340986f4da84135b761d9
enersesian <en6@njit.edu>
2019-11-18 19:13:49 -0500

    Setting up data analytics for bubble sort prototype

--- Assets/Scripts/002Level_BubbleSort/BubbleSortLevel.cs
+++ Assets/Scripts/002Level_BubbleSort/BubbleSortLevel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +9,70 @@ public class BubbleSortLevel : MonoBehaviour {
     public BubbleSortState currentState;
     public float resetTimer = 2f;
 
+    //used for data collection
+    [SerializeField]
+    private Transform centerEyeAnchor, leftHandAnchor, rightHandAnchor;
+    [SerializeField]
+    private bool shouldRecordUserData;
+    private float textRecordTimer;
+    private string recordingValues;
+
     // Use this for initialization
     void Start()
     {
         //SetAppState(BubbleSortState.IntroductionToNextButton);
         SetAppState(currentState);
+
+        //User data collection initialization
+        if (shouldRecordUserData)
+        {
+            //Start recording text data, user started level and tutorial task 00
+            HCInvestigatorManager.instance.StartRecordingText(0);
+            HCInvestigatorManager.instance.StartRecordingText(1);
+            HCInvestigatorManager.instance.WriteTextData(0, "-----------User started bubble sort level with tutorial at " + DateTime.Now.ToString("hh:mm:ss") + "------------");
+            HCInvestigatorManager.instance.WriteTextData(1, "-----------User started bubble sort level with tutorial at " + DateTime.Now.ToString("hh:mm:ss") + "------------");
+            HCInvestigatorManager.instance.WriteTextData(1, ",,Head Position,,,Head Rotation,,,Left Hand Position,,,Right Hand Position");
+            HCInvestigatorManager.instance.WriteTextData(1, "Time, X, Y, Z, X, Y, Z, X, Y, Z, X, Y, Z, Task");
+            HCInvestigatorManager.instance.StartRecordingVideo();
+            textRecordTimer = Time.time;
+        }
+    }
+
+    private void Update()
+    {
+        //record head position/rotation and hand positions every 0.5 seconds
+        if (Time.time - textRecordTimer >= 0.5f && shouldRecordUserData)
+        {
+            recordingValues = DateTime.Now.ToString("hh:mm:ss:ff") + "," +
+                centerEyeAnchor.position.x.ToString("F3") + "," + centerEyeAnchor.position.y.ToString("F3") + "," + centerEyeAnchor.position.z.ToString("F3") + "," +
+                centerEyeAnchor.rotation.eulerAngles.x.ToString("F3") + "," + centerEyeAnchor.rotation.eulerAngles.y.ToString("F3") + "," + centerEyeAnchor.rotation.eulerAngles.z.ToString("F3") + "," +
+                leftHandAnchor.position.x.ToString("F3") + "," + leftHandAnchor.position.y.ToString("F3") + "," + leftHandAnchor.position.z.ToString("F3") + "," +
+                rightHandAnchor.position.x.ToString("F3") + "," + rightHandAnchor.position.y.ToString("F3") + "," + rightHandAnchor.position.z.ToString("F3") + "," + currentState.ToString();
+
+            HCInvestigatorManager.instance.WriteTextData(1, recordingValues);
+            textRecordTimer = Time.time;
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Alpha1)) SetAppState(BubbleSortState.IntroductionToNextButton);
+        //if (Input.GetKeyDown(KeyCode.Alpha2)) SetAppState(BubbleSortState.BeginnerBubbleSortTask01);
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        //pause recordings if user temporary leaves app
+        if (shouldRecordUserData)
+        {
+            if (pause)
+            {
+                HCInvestigatorManager.instance.StopRecordingText(0, "TextLog.txt");
+                HCInvestigatorManager.instance.StopRecordingText(1, "PositionAndRotationLog.csv");
+            }
+            else
+            {
+                HCInvestigatorManager.instance.StartRecordingText(0);
+                HCInvestigatorManager.instance.StartRecordingText(1);
+            }
+        }
     }
 
     public void RegisterListener(Listener newListener)
@@ -23,6 +83,7 @@ public class BubbleSortLevel : MonoBehaviour {
 
     private void SetAppState(BubbleSortState tempState)
     {
+        if(shouldRecordUserData) HCInvestigatorManager.instance.WriteTextData(0, "User went to state " + tempState.ToString() + " at " + DateTime.Now.ToString("hh:mm:ss"));
         currentState = tempState;
         Debug.Log(currentState.ToString());
         foreach (Listener listenerObj in listeners) listenerObj.SetListenerState(tempState);
@@ -62,13 +123,6 @@ public class BubbleSortLevel : MonoBehaviour {
     public void TaskUnsuccessful(int hint)
     {
         foreach (Listener listenerObj in listeners) listenerObj.TaskUnsuccessful(hint);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) SetAppState(BubbleSortState.IntroductionToNextButton);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) SetAppState(BubbleSortState.BeginnerBubbleSortTask01);
     }
 
     private void SetAppStateToIntroductionToCyclingThroughList()
