95d315f4a22cd0a47ea6a8472dd4fda599a6d056
enersesian <en6@njit.edu>
2020-10-18 19:47:11 -0400

    Created WebGLBuild scene for teaching purposes

--- Assets/Scripts/002Level_BubbleSort/MouseClickRayCaster.cs
+++ Assets/Scripts/002Level_BubbleSort/MouseClickRayCaster.cs
@@ -7,14 +7,16 @@ public class MouseClickRayCaster : MonoBehaviour {
     public LayerMask clickMask;
     public Transform cubeTransform;
 
+    Vector3 clickPosition;
+    Ray ray;
+    RaycastHit hit; //dont have to assign this as the raycast will assign this procedurally
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            Vector3 clickPosition = -Vector3.one;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //dont need a z distance
-            RaycastHit hit; //dont have to assign this as the raycast will assign this procedurally
-
+            clickPosition = -Vector3.one;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition); //dont need a z distance
             if (Physics.Raycast(ray, out hit, 100f, clickMask)) //need to add max distance, and layerMask
             {
                 clickPosition = hit.point;
